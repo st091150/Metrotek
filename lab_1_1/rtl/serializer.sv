9e35c62bb18af7eb3094e217f3566c7f77f75a3c
Артём Мещерин <72318993+st091150@users.noreply.github.com>
2025-11-23 06:51:23 +0500

    Add files via upload

--- lab_1_1/rtl/serializer.sv
+++ lab_1_1/rtl/serializer.sv
@@ -1,5 +1,6 @@
 module serializer (
   input  logic        clk_i,
+
   input  logic        srst_i,
 
   input  logic [15:0] data_i,
@@ -8,70 +9,97 @@ module serializer (
 
   output logic        ser_data_o,
   output logic        ser_data_val_o,
+
   output logic        busy_o
 );
 
   logic [15:0] shift_reg;
+
+  logic [4:0]  val_bits_init;
   logic [4:0]  val_bits;
+
   logic        working;
 
-  function automatic logic [4:0] calc_val_bits(input logic [3:0] mod);
-      if (mod == 0)
-          return 5'd16;
-      else if (mod <= 16)
-          return mod;
+  function automatic logic [4:0] calc_val_bits( input logic [3:0] mod );
+      if ( mod == 0 )
+        return 5'd16;
+      else if ( mod <= 2 )
+        return 0;
       else
-          return 5'd16;
+        return mod;
   endfunction
+
+  assign val_bits_init = calc_val_bits( data_mod_i );
+
+  assign busy_o = working;
 
   always_ff @( posedge clk_i )
     begin
-      if (srst_i) 
+      if ( srst_i )
+        val_bits <= '0;
+      else
+        if ( !working )
+          begin
+            if ( data_val_i && val_bits_init )
+              val_bits <= val_bits_init - 5'd1;
+          end
+        else
+          val_bits <= val_bits - 5'd1;
+    end
+
+  always_ff @( posedge clk_i )
+    begin
+      if ( srst_i )
+        working <= 0;
+      else
+        if ( !working )
+          begin
+            if ( data_val_i && val_bits_init )
+              working <= 1;
+          end
+        else
+          if ( !val_bits )
+            working <= 0;
+    end
+
+  always_ff @( posedge clk_i )
+    begin
+      if ( srst_i )
+        shift_reg <= '0;
+      else
+        if ( !working )
+          begin
+            if ( data_val_i && val_bits_init )
+              shift_reg <= ( data_i << 1 );
+          end
+        else
+          shift_reg <= {shift_reg[14:0], 1'b0};
+    end
+
+  always_ff @( posedge clk_i )
+    begin
+      if ( srst_i )
         begin
-          shift_reg       <= '0;
-          val_bits        <= '0;
-          working         <= '0;
-          ser_data_o      <= '0;
-          ser_data_val_o  <= '0;
+          ser_data_val_o <= 0;
+          ser_data_o     <= 0;
         end
       else
         begin
-          if (!working)
+          if ( !working )
             begin
-              if (data_val_i)
+              if ( data_val_i && val_bits_init )
                 begin
-                  logic [4:0] new_val_bits;
-                  new_val_bits = calc_val_bits(data_mod_i);
-                  if (new_val_bits != 0)
-                    begin
-                      working   <= 1;
-
-                      ser_data_o     <= data_i[15];
-                      ser_data_val_o <= 1;
-
-                      val_bits  <= new_val_bits - 5'd1;
-
-                      shift_reg <= {data_i[14:0], 1'b0};
-                    end
+                  ser_data_val_o <= 1;
+                  ser_data_o     <= data_i[15];
                 end
             end
           else
             begin
               ser_data_o <= shift_reg[15];
-
-              val_bits   <= val_bits - 5'd1;
-
-              shift_reg  <= {shift_reg[14:0], 1'b0};
-
-              if (val_bits == 0)
-                begin
-                  working        <= 0;
-                  ser_data_val_o <= 0;
-                end
+              if ( !val_bits )
+                ser_data_val_o <= 0;
             end
         end
     end
-
-  assign busy_o = working;
 
 endmodule
--- lab_1_1/tb/serializer_tb.sv
+++ lab_1_1/tb/serializer_tb.sv
@@ -4,21 +4,28 @@ module serializer_tb;
 
   parameter TEST_NUM = 20;
 
-  logic         clk;
+  bit          clk;
 
-  logic         srst_i;
-  logic [15:0]  data_i;
-  logic [3:0]   data_mod_i;
-  logic         data_val_i;
+  logic        srst_i;
 
-  logic         ser_data_o;
-  logic         ser_data_val_o;
-  logic         busy_o;
+  logic [15:0] data_i;
+  logic [3:0]  data_mod_i;
+  logic        data_val_i;
 
-  logic [15:0]  data_i_tb;
-  logic [15:0]  expected_data_o;
-  logic [15:0]  output_data_o;
+  logic        ser_data_o;
+  logic        ser_data_val_o;
 
+  logic        busy_o;
+
+  logic [15:0] data_i_local;
+  logic [15:0] data_i_fixed;
+  logic [3:0]  data_mod_i_local;
+  logic        data_val_i_local;
+
+  logic [15:0] expected_data_o;
+  logic [15:0] output_data_o;
+
+  int           num_bits;
   serializer DUT (
     .clk_i          ( clk            ),
     .srst_i         ( srst_i         ),
@@ -30,91 +37,115 @@ module serializer_tb;
     .busy_o         ( busy_o         )
   );
 
+  always #5 clk = ~clk;
+
   initial 
     begin
-      clk             =  0;
-
-      srst_i          =  1;
-      data_i          = '0;
-      data_mod_i      = '0;
-      data_val_i      =  0;
-
-      data_i_tb       = '0;
-      expected_data_o = '0;
-      output_data_o   = '0;
+      forever 
+        begin
+          @(posedge clk) 
+          data_i_local <= $urandom_range(16'hFFFF, 0);
+        end
     end
 
-  always
-    begin
-      #5 
-      clk    = ~clk;
-      data_i = $urandom_range(16'hFFFF, 0);
-    end
-
-  
   clocking cb @(posedge clk);
     output data_i;
     output data_mod_i;
+    output data_val_i;
+
     output srst_i;
 
     input ser_data_o;
     input ser_data_val_o;
+
     input busy_o;
   endclocking
 
   initial 
     begin
-      int num_bits;
-      for( int i = 0; i < TEST_NUM; i = i + 1 )
+      srst_i           =  1;
+      
+      data_i           =  0;
+      data_mod_i       = '0;
+      data_val_i       =  0;
+
+      data_i_local     = '0;
+      data_i_fixed     = '0;
+      data_mod_i_local = '0;
+      data_val_i_local =  0;
+
+      for( int i = 0; i < TEST_NUM; i++ )
         begin
+          expected_data_o = '0;
+          output_data_o   = '0;
+
           @( cb )
           cb.srst_i <= 1;
+
           @( cb );
           cb.srst_i <= 0;
           $display("TEST %0d", i);
 
-          data_mod_i = $urandom_range(15,0);
-          data_val_i = $urandom_range(1, 0);
+          data_mod_i_local = $urandom_range(15, 0);
+          data_val_i_local = $urandom_range(1, 0);
 
-          @( cb )
-          cb.data_i     <= data_i;
-          cb.data_mod_i <= data_mod_i;
+          @ ( cb );
+          cb.data_i     <= data_i_local;
+          cb.data_mod_i <= data_mod_i_local;
+          cb.data_val_i <= data_val_i_local;
 
-          output_data_o   = '0;
-          data_i_tb       = '0;
-          expected_data_o = '0;
-          
-          if( data_val_i == 1 )
+          if( cb.busy_o === 1 )
             begin
-              data_i_tb = data_i;
-              if ( data_mod_i == 0 )
-                expected_data_o = data_i_tb;
+              $error("ERROR: Expected busy_o = 0, DUT busy_o = 1");
+              $stop;
+            end
+
+          if( data_val_i_local )
+            begin
+              data_i_fixed = data_i_local;
+
+              if ( data_mod_i_local == 0 )
+                expected_data_o = data_i_fixed;
+              else if ( data_mod_i_local <= 2 )
+                expected_data_o = '0;
               else
-                for ( int k = 0; k < data_mod_i; k++ ) 
-                    expected_data_o[15-k] = data_i_tb[15-k];
+                for ( int k = 0; k < data_mod_i_local; k++ ) 
+                    expected_data_o[15-k] = data_i_fixed[15-k];
             end
           else
             expected_data_o = '0;
-
-          num_bits = (data_mod_i == 0) ? 16 : data_mod_i;
-          for (int t = 0; t < num_bits; t++) 
-            begin
-              @(cb);
-              if (cb.ser_data_val_o === 1 && cb.busy_o === 1)
-                  output_data_o[15 - t] = cb.ser_data_o;
-            end
+          
+          @( cb )
+            cb.data_val_i <= 0;
 
           $display("data_i = %b, data_mod_i = %d, data_val_i = %d, expected_data_o = %b",
-                    data_i_tb, data_mod_i, data_val_i, expected_data_o);
+                    data_i_fixed, data_mod_i_local, data_val_i_local, expected_data_o);
 
-          for ( int k = 0; k < data_mod_i ; k++ ) 
+          num_bits = ( data_mod_i_local == 0 ) ? 16 : data_mod_i_local;
+
+          for (int t = 0; t < num_bits; t++) 
             begin
-            if ( expected_data_o[15 - k] != output_data_o[15 - k] )
-              begin
-                $error("ERROR: Expected data_o = %b, DUT data_o = %b",
-                        expected_data_o, output_data_o);
-                $stop;
-              end
+              @( cb );
+              if ( data_val_i_local && ( data_mod_i_local !== 1 ) && ( data_mod_i_local !== 2 ) && ( cb.busy_o === 0 ) )
+                begin
+                  $error("ERROR: Expected busy_o = 1, DUT busy_o = 0");
+                  $stop;
+                end
+              if ( cb.ser_data_val_o === 1 )
+                begin
+                  if ( ( num_bits == 1 ) || ( num_bits == 2 ) )
+                    begin
+                      $error("ERROR: Expected ser_data_val_o = 0 when data_mod_i in [1, 2], DUT ser_data_val_o = 1");
+                      $stop;
+                    end
+                  output_data_o[15 - t] = cb.ser_data_o;
+                  if( output_data_o[15 - t] != expected_data_o[15 - t] )
+                    begin
+                      $error("ERROR: Expected data_o = %b, DUT data_o = %b, on idx=%d",
+                        expected_data_o, output_data_o, 15 - t);
+                      $stop;
+                    end
+                end
             end
         end
         $display("ALL TESTS PASSED");

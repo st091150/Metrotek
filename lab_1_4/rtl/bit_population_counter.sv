b8ff888c76b4547f00622b761a2e6d12cd7c9789
Артём Мещерин <72318993+st091150@users.noreply.github.com>
2025-12-05 17:23:51 +0500

    Add files via upload

--- lab_1_4/rtl/bit_population_counter.sv
+++ lab_1_4/rtl/bit_population_counter.sv
@@ -19,27 +19,18 @@ module bit_population_counter #(
       if ( srst_i )
         data_val_o <= 1'b0;
       else
-        begin
-          data_val_o <= ( !data_val_o && data_val_i );
-          data_o     <= bit_counter;
-        end
+        data_val_o <= data_val_i;
     end
+
+  always_ff @( posedge clk_i )
+    data_o <= bit_counter;
 
   always_comb
     begin
-      logic [WIDTH-1:0] data_i_local;
-    
-      data_i_local = data_i;
-      bit_counter  = '0;
+      bit_counter = '0;
       
-      if ( data_val_i )
-        begin
-          for ( int i = 0; i < WIDTH; i++ ) 
-            begin
-              if ( data_i_local[i] == 1'b1 )
-                bit_counter = bit_counter + 1'b1;
-            end
-        end
+      for ( int i = 0; i < WIDTH; i++ ) 
+        bit_counter = bit_counter + data_i[i];
     end
 
 endmodule
--- lab_1_4/tb/bit_population_counter_tb.sv
+++ lab_1_4/tb/bit_population_counter_tb.sv
@@ -2,8 +2,10 @@
 
 module bit_population_counter_tb;
 
-  parameter WIDTH = 16;
-  parameter TEST_NUM = 1000;
+  parameter WIDTH        = 16;
+  parameter SEQ_TEST_NUM = 100;
+  parameter TEST_NUM     = 1000;
+
 
   bit                     clk;
 
@@ -38,6 +40,69 @@ module bit_population_counter_tb;
     input  data_val_o;
   endclocking
 
+task automatic count_ones_random_seq_test(
+    input int test_num
+    );
+    
+    logic [WIDTH-1:0] data_i_local;
+
+    logic [$clog2(WIDTH):0] expected_bit_count;
+
+    data_i_local = $urandom_range( ( 2**WIDTH ) - 1, 0);
+
+    cb.data_i     <= data_i_local;
+    cb.data_val_i <= 1;
+
+    @( cb );
+    if ( cb.data_val_o === 1 )
+      begin
+        $error("ERROR: expected data_val_o: 0, DUT data_val_o: 1");
+        $stop;
+      end
+
+    for ( int i = 0; i < test_num; i++ )
+      begin
+        $display("TEST (random_seq): %0d", i);
+        $display("data_i=%b data_val_i=%b", data_i_local, 1);
+
+        expected_bit_count = $countones(data_i_local);
+
+        data_i_local = $urandom_range( ( 2**WIDTH ) - 1, 0);
+
+        cb.data_i <= data_i_local;
+
+        @( cb );
+        if ( cb.data_val_o === 0 )
+          begin
+            $error("ERROR: expected data_val_o: 1, DUT data_val_o: 0");
+            $stop;
+          end
+
+        if ( cb.data_o !== expected_bit_count )
+          begin
+            $error("ERROR: expected bit count: %d, DUT bit count: %d", expected_bit_count, cb.data_o);
+            $stop;
+          end
+      end
+
+    cb.data_val_i <= 0;
+
+    @( cb );
+    if ( cb.data_val_o === 0 )
+      begin
+        $error("ERROR: expected data_val_o: 1, DUT data_val_o: 0");
+        $stop;
+      end
+
+    @( cb );
+    if ( cb.data_val_o === 1 )
+      begin
+        $error("ERROR: expected data_val_o: 0, DUT data_val_o: 1");
+        $stop;
+      end
+
+  endtask
+
 task automatic count_ones_test(
     input logic [WIDTH-1:0] data_i_in,
     input bit               data_val_i_in
@@ -51,12 +116,14 @@ task automatic count_ones_test(
     expected_bit_count = $countones(data_i_in);
 
     @( cb );
-    if ( cb.data_val_o === 1)
+    if ( cb.data_val_o !== 0)
       begin
         $error("ERROR: expected data_val_o: 0, DUT data_val_o: 1");
         $stop;
       end
-    
+
+    cb.data_val_i <= 0;
+
     @( cb );
     if ( cb.data_val_o !== data_val_i_in )
       begin
@@ -91,9 +158,12 @@ task automatic count_ones_test(
       @( cb );
       cb.srst_i <= 0;
 
+      $display("TEST (seq)");
+      count_ones_random_seq_test(SEQ_TEST_NUM);
+
       for( int i = 0; i < TEST_NUM; i++ )
         begin
-          $display("TEST %0d", i);
+          $display("TEST (random) %0d", i);
 
           data_i_local     = $urandom_range( ( 2**WIDTH ) - 1, 0);
           data_val_i_local = $urandom_range(1, 0);
@@ -102,6 +172,7 @@ task automatic count_ones_test(
 
           count_ones_test(data_i_local, data_val_i_local);
         end
+        
         $display("ALL TESTS PASSED");
         $stop;
       end
